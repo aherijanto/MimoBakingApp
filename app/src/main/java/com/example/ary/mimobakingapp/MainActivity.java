1e9f10c4fae54dcc01a4f656da1103072d2cd8be
Ary Herijanto <aherijanto@gmail.com>
2017-09-15 00:00:34 +0700

    rewritecode

--- app/src/main/java/com/example/ary/mimobakingapp/MainActivity.java
+++ app/src/main/java/com/example/ary/mimobakingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ary.mimobakingapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,8 +10,11 @@ import android.util.Log;
 import com.example.ary.mimobakingapp.API.RecipeInterface;
 import com.example.ary.mimobakingapp.Adapter.RecipeMainAdapter;
 import com.example.ary.mimobakingapp.Model.RecipeJSONResponse;
-import com.example.ary.mimobakingapp.Model.RecipeMain;
+import com.example.ary.mimobakingapp.Utilities.ConnectUtils;
+import com.example.ary.mimobakingapp.Utilities.RecipeOnJSON;
 
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,7 +28,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvRecipeMain;
-    private ArrayList<RecipeMain> myRecipeMain;
+
     private RecipeMainAdapter adapter;
 
     @Override
@@ -32,39 +36,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        initViews();
-    }
-
-    private void initViews(){
         rvRecipeMain = (RecyclerView)findViewById(R.id.rv_page_1);
         rvRecipeMain.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         rvRecipeMain.setLayoutManager(layoutManager);
-        loadJSON();
+
+        new FetchRecipe().execute();
+
     }
 
-    private void loadJSON() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        RecipeInterface request = retrofit.create(RecipeInterface.class);
-        Call<RecipeJSONResponse> call = request.getJSON();
-        call.enqueue(new Callback<RecipeJSONResponse>() {
-            @Override
-            public void onResponse(Call<RecipeJSONResponse> call, Response<RecipeJSONResponse> response) {
+    public class FetchRecipe extends AsyncTask<String,Void,String[]>{
 
-                RecipeJSONResponse jsonResponse = response.body();
-                myRecipeMain = new ArrayList<RecipeMain>(Arrays.asList(jsonResponse.getMyRecipe()));
-                adapter = new RecipeMainAdapter(myRecipeMain);
-                rvRecipeMain.setAdapter(adapter);
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
             }
 
-            @Override
-            public void onFailure(Call<RecipeJSONResponse> call, Throwable t) {
-                Log.d("Error", t.getMessage());
+            String me = params[0];
+            URL RecipeRequestUrl = ConnectUtils.buildURL(me);
+
+            try {
+                String RecipeResponse = ConnectUtils
+                        .getResponseFromHttpUrl(RecipeRequestUrl);
+
+                String[] JsonRecipeData = RecipeOnJSON.getRecipeName(MainActivity.this,RecipeResponse);
+
+
+                return JsonRecipeData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
             }
-        });
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+
+                for (String recipeString : strings) {
+                    rvRecipeMain.setAdapter(new RecipeMainAdapter(getApplicationContext(), recipeString));
+                }
+            }
+        }
     }
+
+
+
 
 }
