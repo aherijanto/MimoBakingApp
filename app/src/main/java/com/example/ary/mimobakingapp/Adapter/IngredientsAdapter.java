4b43c7d7b47025d2f6a9c3621453d51d8dd66c44
Ary Herijanto <aherijanto@gmail.com>
2017-09-17 11:33:38 +0700

    rewritecode

--- app/src/main/java/com/example/ary/mimobakingapp/Adapter/IngredientsAdapter.java
+++ app/src/main/java/com/example/ary/mimobakingapp/Adapter/IngredientsAdapter.java
@@ -6,36 +6,36 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.ary.mimobakingapp.MainActivitySteps;
-
-import com.example.ary.mimobakingapp.Model.Ingredients;
 import com.example.ary.mimobakingapp.Model.Recipe;
 import com.example.ary.mimobakingapp.R;
 
 import java.util.ArrayList;
 
 /**
- * Created by ary on 9/15/17.
+ * Created by ary on 9/17/17.
  */
 
-public class RecipeMainAdapter extends RecyclerView.Adapter<RecipeMainAdapter.MyViewHolder>{
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.MyViewHolder> {
+
 
     private final ArrayList<Recipe> rvRecipeList;
 
     Context context;
 
 
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         public TextView recipeName;
 
+
         public MyViewHolder(View view){
             super(view);
             recipeName=(TextView) view.findViewById(R.id.recipename);
+
 
             view.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -64,25 +64,24 @@ public class RecipeMainAdapter extends RecyclerView.Adapter<RecipeMainAdapter.My
     }
 
 
-    public RecipeMainAdapter(Context context, ArrayList<Recipe> rvRecipeList) {
+    public IngredientsAdapter(Context context, ArrayList<Recipe> rvRecipeList) {
         this.rvRecipeList = rvRecipeList;
         this.context = context;
     }
 
 
     @Override
-    public RecipeMainAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public IngredientsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recipe_main_card, parent, false);
-        return new MyViewHolder(itemView);
+        return new IngredientsAdapter.MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(IngredientsAdapter.MyViewHolder holder, int position) {
         TextView myRecipeName=holder.recipeName;
         myRecipeName.setText(rvRecipeList.get(position).getName());
-        //String vote=Double.toString(rvRecipeList.get(position).getVoteAverage());
-        //viewHolder.userrating.setText(vote);
+
     }
 
     @Override
@@ -90,7 +89,5 @@ public class RecipeMainAdapter extends RecyclerView.Adapter<RecipeMainAdapter.My
         return rvRecipeList.size();
     }
 
-
-
-    }
+}
 
