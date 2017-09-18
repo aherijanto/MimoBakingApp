7bef2cf1e61c8ab9790094041c5deee9aa0aef0a
Ary Herijanto <aherijanto@gmail.com>
2017-09-18 20:24:39 +0700

    rewritecode

--- app/src/main/java/com/example/ary/mimobakingapp/DetailFragment.java
+++ app/src/main/java/com/example/ary/mimobakingapp/DetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.ary.mimobakingapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -9,15 +10,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlaybackControlView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 /**
  * Created by ary on 9/17/17.
@@ -26,6 +45,8 @@ import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 public class DetailFragment extends Fragment {
 
     private TextView mTextView;
+    private SimpleExoPlayerView simpleExoPlayerView;
+    private SimpleExoPlayer player;
 
 
     @Nullable
@@ -33,25 +54,36 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.simple_exo_player_view, container, false);
         mTextView = (TextView) rootView.findViewById(R.id.textSteps);
-        simpleExoPlayerView=(SimpleExoPlayer) rootView.findViewById(R.id.player);
+
 
         if (getArguments() != null) {
             String myDesc = getArguments().getString("mydesc");
             String myvideo= getArguments().getString("myvideourl");
             mTextView.setText(myDesc);
 
-            Handler mainHandler = new Handler();
-            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-            TrackSelection.Factory videoTrackSelectionFactory =
-                    new AdaptiveTrackSelection.Factory(bandwidthMeter);
-            TrackSelector trackSelector =
-                    new DefaultTrackSelector(videoTrackSelectionFactory);
 
+                BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+                TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+                TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
 
-            SimpleExoPlayer player =
-                    ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector);
+                LoadControl loadControl = new DefaultLoadControl();
 
-            simpleExoPlayerView.setPlayer(player);
+                player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+                simpleExoPlayerView = new SimpleExoPlayerView(getActivity());
+                simpleExoPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.player);
+
+                simpleExoPlayerView.setUseController(true);
+                simpleExoPlayerView.requestFocus();
+
+                simpleExoPlayerView.setPlayer(player);
+
+                Uri mp4VideoUri = Uri.parse(myvideo);
+
+            DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), bandwidthMeterA);
+
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
 
 
