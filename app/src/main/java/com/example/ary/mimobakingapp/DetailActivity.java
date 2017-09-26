bcfcef4c622cfb980fa30ce9a670fc0d34c72ea4
Ary Herijanto <aherijanto@gmail.com>
2017-09-26 19:59:30 +0700

    rewritecode

--- app/src/main/java/com/example/ary/mimobakingapp/DetailActivity.java
+++ app/src/main/java/com/example/ary/mimobakingapp/DetailActivity.java
@@ -11,26 +11,48 @@ import com.example.ary.mimobakingapp.Model.Steps;
  */
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String MY_FRAGMENT_VIDEO = "my_fragment_video";
+    private DetailFragment detailFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         if (getIntent() != null) {
             //Get EXTRA from intent and attach to Fragment as Argument
 
             //receive desc and video string from IngredientsAdapter
             Bundle stepbundle = getIntent().getExtras();
-            String mydesc= stepbundle.getString("desc");
-            String myvideoUrl=stepbundle.getString("videoUrl");
+            String mydesc = stepbundle.getString("desc");
+            String myvideoUrl = stepbundle.getString("videoUrl");
 
             //send desc and video string to Detailfragment
             Bundle args = new Bundle();
             args.putString("mydesc", mydesc);
             args.putString("myvideourl", myvideoUrl);
 
-            DetailFragment detailFragment = new DetailFragment();
-            detailFragment.setArguments(args);
+            if (savedInstanceState == null) {
+                DetailFragment detailFragment = new DetailFragment();
+                detailFragment.setArguments(args);
+
+            }else {
+                detailFragment = (DetailFragment) getSupportFragmentManager().getFragment(savedInstanceState, MY_FRAGMENT_VIDEO);
+            }
+
             getSupportFragmentManager().beginTransaction().replace(R.id.detailContainer, detailFragment).commit();
+
+
         }
+
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState){
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, MY_FRAGMENT_VIDEO, detailFragment);
     }
 }
