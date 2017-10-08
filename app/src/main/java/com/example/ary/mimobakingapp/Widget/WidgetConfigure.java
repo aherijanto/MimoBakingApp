791af8d6c7d9e159a3a17a5d2d81c572624751bc
Ary Herijanto <aherijanto@gmail.com>
2017-10-08 21:38:13 +0700

    afterclone

---
+++ app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetConfigure.java
@@ -0,0 +1,10 @@
+package com.example.ary.mimobakingapp.Widget;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ary on 10/8/17.
+ */
+
+public class WidgetConfigure extends AppCompatActivity {
+}
