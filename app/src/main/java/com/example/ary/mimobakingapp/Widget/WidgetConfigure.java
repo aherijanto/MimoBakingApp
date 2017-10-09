795ff87413eadccad003aa8643e35205b9fe5c16
Ary Herijanto <aherijanto@gmail.com>
2017-10-09 15:31:21 +0700

    afterclone

--- app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetConfigure.java
+++ app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetConfigure.java
@@ -1,6 +1,27 @@
 package com.example.ary.mimobakingapp.Widget;
 
+import android.annotation.SuppressLint;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import com.example.ary.mimobakingapp.Model.Recipe;
+import com.example.ary.mimobakingapp.R;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ary on 10/8/17.
