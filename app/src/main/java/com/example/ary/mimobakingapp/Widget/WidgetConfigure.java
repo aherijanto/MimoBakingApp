a5a01e928fbf74a5a3a6ff5c9da3cc416c936d58
Ary Herijanto <aherijanto@gmail.com>
2017-10-09 16:20:32 +0700

    afterclone

--- app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetConfigure.java
+++ app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetConfigure.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.example.ary.mimobakingapp.Adapter.RecipeMainAdapter;
 import com.example.ary.mimobakingapp.Model.Recipe;
 import com.example.ary.mimobakingapp.R;
 import com.google.gson.GsonBuilder;
@@ -23,9 +26,116 @@ import com.google.gson.reflect.TypeToken;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by ary on 10/8/17.
  */
 
 public class WidgetConfigure extends AppCompatActivity {
+    private int mAppWidgetId;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        setContentView(R.layout.widget_recipe);
+        RecyclerView rv = (RecyclerView) findViewById(com.example.ary.mimobakingapp.R.id.rv);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String json = preferences.getString(RecipeMainAdapter.SHARED_PREFS_KEY, "");
+        List<Recipe> recipes = new GsonBuilder().create().fromJson(json
+                , new TypeToken<List<Recipe>>(){}.getType());
+        rv.setAdapter(new RecyclerViewWidgetAdapter(recipes));
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private void execute(Recipe recipe){
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        RemoteViews views = initViews(this, appWidgetManager, mAppWidgetId, recipe);
+        appWidgetManager.updateAppWidget(mAppWidgetId, views);
+
+        Intent resultValue = new Intent(this, MyWidgetRemoteViewsService.class);
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+
+        setResult(RESULT_OK, resultValue);
+
+        finish();
+    }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    private RemoteViews initViews(Context context,
+                                  AppWidgetManager widgetManager, int widgetId, Recipe recipe) {
+
+        RemoteViews mView = new RemoteViews(context.getPackageName(),
+                R.layout.widget_layout);
+
+        Intent intent = new Intent(context,MyWidgetRemoteViewsService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+        String sRecipe = new GsonBuilder().create().toJson(recipe);
+
+        intent.putExtra(WidgetDataProvider.SELECTED_RECIPE, sRecipe);
+        mView.setRemoteAdapter(widgetId, R.id.widget_list_view, intent);
+
+        return mView;
+    }
+
+
+    class RecyclerViewWidgetAdapter extends RecyclerView.Adapter<RecyclerViewWidgetAdapter.WidgetAdapter>{
+
+        private Context context;
+        List<Recipe> recipes = new ArrayList<>();
+
+        public RecyclerViewWidgetAdapter(List<Recipe> recipes){
+            this.recipes = recipes;
+        }
+
+        @Override
+        public WidgetAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
+            context = parent.getContext();
+            View view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+            WidgetAdapter widgetAdapter = new WidgetAdapter(view);
+            return widgetAdapter ;
+        }
+
+        @Override
+        public void onBindViewHolder(WidgetAdapter holder, int position) {
+            final Recipe recipe = recipes.get(position);
+            holder.tv.setText(recipe.getName());
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    execute(recipe);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            if (recipes != null)
+                return recipes.size();
+            else return 0;
+        }
+
+        class WidgetAdapter extends RecyclerView.ViewHolder{
+            @BindView(android.R.id.text1) TextView tv;
+            public WidgetAdapter(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/ary/mimobakingapp/Widget/WidgetDataProvider.java
@@ -0,0 +1,88 @@
+package com.example.ary.mimobakingapp.Widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.ary.mimobakingapp.Model.Ingredients;
+import com.example.ary.mimobakingapp.Model.Recipe;
+import com.example.ary.mimobakingapp.R;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.ary.mimobakingapp.R.id.ingredients;
+
+/**
+ * Created by ary on 10/9/17.
+ */
+
+public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
+
+    public static final String SELECTED_RECIPE = "com.example.ary.mimobakingapp.SELECTED_RECIPE";
+
+    private Context context;
+    private Intent intent;
+    private List<Ingredients> ingredientList = new ArrayList<>();
+
+    public WidgetDataProvider(Context context, Intent intent){
+        this.context = context;
+        this.intent = intent;
+    }
+
+    void initData(){
+        String sRecipe = intent.getStringExtra(SELECTED_RECIPE);
+        Recipe recipe = new GsonBuilder().create().fromJson(sRecipe, Recipe.class);
+        ingredientList.addAll(recipe.getIngredients());
+    }
+
+    @Override
+    public void onCreate() {
+        initData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return ingredientList.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        Ingredients ingredient = ingredientList.get(position);
+        RemoteViews views = new RemoteViews(context.getPackageName(), android.R.layout.simple_list_item_1);
+        views.setTextViewText(android.R.id.text1, String.format(context.getString(R.string.ingredients_detail)
+                , ingredient.getQuantity(),ingredient.getMeasure(), ingredient.getIngredient()));
+        return views;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
